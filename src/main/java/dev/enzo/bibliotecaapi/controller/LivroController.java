39864fa6d99e2859238b3e5dc701c83ee0fa891d
enzosdev <enzohfs@gmail.com>
2025-06-28 12:10:32 -0300

    feat: adiciona a logica de negocio no Controller e no Service da entidade Livro

--- src/main/java/dev/enzo/bibliotecaapi/controller/LivroController.java
+++ src/main/java/dev/enzo/bibliotecaapi/controller/LivroController.java
@@ -1,10 +1,64 @@
 package dev.enzo.bibliotecaapi.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.enzo.bibliotecaapi.dto.LivroDTO;
+import dev.enzo.bibliotecaapi.service.LivroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/livro")
 public class LivroController {
+    private LivroService livroService;
+
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
+    @PostMapping("/adicionar")
+    public ResponseEntity<String> adicionarLivro(@RequestBody LivroDTO livroDTO){
+        LivroDTO livrosDTO = livroService.adicionarLivros(livroDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(livrosDTO.getTitulo());
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<LivroDTO>> listarLivros(){
+        List<LivroDTO> livros = livroService.listarLivros();
+        return ResponseEntity.ok(livros);
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<String> atualizarLivro(@PathVariable  Long id, @RequestBody LivroDTO livroAtualizado){
+        if(livroService.listarLivrosPorId(id) != null){
+            LivroDTO livroDTO = livroService.atualizarLivroPorId(id, livroAtualizado);
+            return ResponseEntity.ok("Livro de ID " + livroDTO.getId() + " Atualizado com Sucesso.");
+
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O livro de ID " + id + " nao encontrado.");
+        }
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?>listarPorId(@PathVariable Long id){
+        if(livroService.listarLivrosPorId(id) != null){
+            LivroDTO livroDTO = livroService.listarLivrosPorId(id);
+            return ResponseEntity.status(HttpStatus.FOUND).body("Livro: " + livroDTO.getTitulo());
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O livro de ID " + id + " nao encontrado.");
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarPorId(@PathVariable Long id){
+        if (livroService.listarLivrosPorId(id) != null){
+           livroService.deletarLivroPorId(id);
+            return ResponseEntity.ok("O livro de id " + id + " deletado com sucesso");
+
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("O livro de ID " + id + " nao foi encontrado." );
+        }
+    }
 }
--- src/main/java/dev/enzo/bibliotecaapi/service/LivroService.java
+++ src/main/java/dev/enzo/bibliotecaapi/service/LivroService.java
@@ -1,12 +1,57 @@
 package dev.enzo.bibliotecaapi.service;
 
 
+import dev.enzo.bibliotecaapi.dto.LivroDTO;
 import dev.enzo.bibliotecaapi.mapper.LivroMapper;
+import dev.enzo.bibliotecaapi.model.Livro;
 import dev.enzo.bibliotecaapi.repository.LivroRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class LivroService {
     private LivroRepository livroRepository;
     private LivroMapper livroMapper;
+
+    public LivroService(LivroRepository livroRepository, LivroMapper livroMapper) {
+        this.livroRepository = livroRepository;
+        this.livroMapper = livroMapper;
+    }
+
+
+    public List<LivroDTO> listarLivros(){
+        List<Livro> livros = livroRepository.findAll();
+        return livros.stream()
+                .map(livroMapper::map)
+                .collect(Collectors.toList());
+    }
+
+        public LivroDTO adicionarLivros(LivroDTO livroDTO){
+            Livro novoLivro = livroMapper.map(livroDTO);
+            novoLivro = livroRepository.save(novoLivro);
+            return livroMapper.map(novoLivro);
+        }
+
+    public LivroDTO listarLivrosPorId(Long id){
+        Optional<Livro> livrosPorId = livroRepository.findById(id);
+        return livrosPorId.map(livroMapper::map).orElse(null);
+    }
+
+    public void deletarLivroPorId(Long id){
+        livroRepository.deleteById(id);
+    }
+
+    public LivroDTO atualizarLivroPorId(Long id, LivroDTO livroDTO){
+        Optional<Livro> livroExistente = livroRepository.findById(id);
+        if(livroExistente.isPresent()){
+            Livro livroAtualizado = livroMapper.map(livroDTO);
+            livroAtualizado.setId(id);
+            Livro livroSalvo = livroRepository.save(livroAtualizado);
+            return livroMapper.map(livroSalvo);
+        }
+        return null;
+    }
 }
