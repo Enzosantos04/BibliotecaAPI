29a2f33af11ff71ed81a1d32602ca89e9f03520c
enzosdev <enzohfs@gmail.com>
2025-07-01 08:41:08 -0300

    fix: Corrige o UsuarioRepository

--- src/main/java/dev/enzo/bibliotecaapi/repository/UsuarioRepository.java
+++ src/main/java/dev/enzo/bibliotecaapi/repository/UsuarioRepository.java
@@ -3,5 +3,5 @@ package dev.enzo.bibliotecaapi.repository;
 import dev.enzo.bibliotecaapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsurarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
