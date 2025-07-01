68923f1e914001c24f6c645dd3955a74c7113b68
enzosdev <enzohfs@gmail.com>
2025-07-01 09:04:38 -0300

    feat: cria regra de negocio no UsuarioService

--- src/main/java/dev/enzo/bibliotecaapi/service/UsuarioService.java
+++ src/main/java/dev/enzo/bibliotecaapi/service/UsuarioService.java
@@ -1,4 +1,63 @@
 package dev.enzo.bibliotecaapi.service;
 
+import dev.enzo.bibliotecaapi.dto.UsuarioDTO;
+import dev.enzo.bibliotecaapi.mapper.UsuarioMapper;
+import dev.enzo.bibliotecaapi.model.Usuario;
+
+import dev.enzo.bibliotecaapi.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
+    private UsuarioMapper usuarioMapper;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, UsuarioMapper usuarioMapper) {
+        this.usuarioRepository = usuarioRepository;
+        this.usuarioMapper = usuarioMapper;
+    }
+
+    public List<UsuarioDTO> listarUsuarios(){
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        return usuarios.stream()
+                .map(usuarioMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public UsuarioDTO adicionarUsuario(UsuarioDTO usuarioDTO){
+        Usuario novoUsuario = usuarioMapper.map(usuarioDTO);
+        novoUsuario = usuarioRepository.save(novoUsuario);
+        return usuarioMapper.map(novoUsuario);
+    }
+
+    public UsuarioDTO listarUsurioPorId(Long id){
+        Optional<Usuario> usuarioPorId = usuarioRepository.findById(id);
+        return usuarioPorId.map(usuarioMapper::map).orElse(null);
+    }
+
+    public void deletarPorId(Long id){
+        usuarioRepository.deleteById(id);
+    }
+
+    public UsuarioDTO atualizarUsuarioPorId(Long id, UsuarioDTO usuarioDTO){
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+        if(usuarioExistente.isPresent()){
+            Usuario usuarioAtualizado = usuarioMapper.map(usuarioDTO);
+            usuarioAtualizado.setId(id);
+            Usuario usuarioSalvo = usuarioRepository.save(usuarioAtualizado);
+            return usuarioMapper.map(usuarioSalvo);
+        }
+        return null;
+    }
+
+
+
+
+
+
+
 }
