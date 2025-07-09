6affda2dcf0e64b3631a78af57736fbb865d8f3d
enzosdev <enzohfs@gmail.com>
2025-07-09 10:31:55 -0300

    feat: adiciona Controller para a entidade Emprestimo

--- src/main/java/dev/enzo/bibliotecaapi/controller/EmprestimoController.java
+++ src/main/java/dev/enzo/bibliotecaapi/controller/EmprestimoController.java
@@ -1,4 +1,39 @@
 package dev.enzo.bibliotecaapi.controller;
 
+
+import dev.enzo.bibliotecaapi.dto.EmprestimoDTO;
+import dev.enzo.bibliotecaapi.service.EmprestimoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("emprestimo")
 public class EmprestimoController {
+    EmprestimoService emprestimoService;
+
+    public EmprestimoController(EmprestimoService emprestimoService) {
+        this.emprestimoService = emprestimoService;
+    }
+
+    @GetMapping("/listar")
+  public ResponseEntity<List<EmprestimoDTO>> listarEmprestimos(){
+        List<EmprestimoDTO> emprestimos = emprestimoService.listarEmprestimos();
+        return ResponseEntity.ok(emprestimos);
+  }
+
+  @PostMapping("/criar")
+    public ResponseEntity<String> criarEmprestimos(@RequestBody EmprestimoDTO emprestimoDTO){
+        EmprestimoDTO emprestimosDTO = emprestimoService.criarEmprestimo(emprestimoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(emprestimosDTO.getDataEmprestimo().toString());
+  }
+
+  @PutMapping("/devolver/{id}")
+  public ResponseEntity<EmprestimoDTO> devolverEmprestimo(@PathVariable Long id) {
+      EmprestimoDTO emprestimoDTO = emprestimoService.devolverEmprestimo(id);
+      return ResponseEntity.ok(emprestimoDTO);
+  }
+
 }
